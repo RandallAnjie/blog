b8b251d259ee59f437e4419ab0b229b6bc546d60
Randall <2227535604@qq.com>
2023-03-28 16:02:41 +0800

    Update cloudflareBuild.sh

--- cloudflareBuild.sh
+++ cloudflareBuild.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # 配置环境
+echo '开始配置环境'
+echo '安装hexo-cli'
 npm install -g hexo-cli
-npm install
+echo '安装完成hexo，设置hexo路径'
+echo 'PATH="$PATH:./node_modules/.bin"' >> ~/.profile
+source ~/.profile
+
+echo '开始部署'
+
 hexo clean
 hexo generate
 hexo deploy
